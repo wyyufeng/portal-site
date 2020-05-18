d3a43832db69a91b268f110dfbda18184c8da6dd
wyyufeng <wyyufeng@gmail.com>
2020-05-18 09:00:51 +0800

    修改 gateway 使用方式

--- packages/portal-data/src/cmsDataProvider.ts
+++ packages/portal-data/src/cmsDataProvider.ts
@@ -3,94 +3,94 @@ import { listMapper, listOneMapper } from './mapper';
 import { CmsDataProvider, IHTTPClient } from '@portal-site/types';
 import { RootRouteMap } from './route/index';
 export * from './httpClient';
-export default (httpClient: IHTTPClient, gateway: string = 'sw'): CmsDataProvider => {
-    return {
-        queryRoutes(resource: string | undefined): Promise<any> {
-            const endPoint = `/${gateway}-cms/api/queryAllChannel/${resource}`;
+export default (httpClient: IHTTPClient, gateway: string = 'sw-cms'): CmsDataProvider => {
+  return {
+    queryRoutes(resource: string | undefined): Promise<any> {
+      const endPoint = `/${gateway}/api/queryAllChannel/${resource}`;
 
-            return new Promise((resolve, reject) => {
-                httpClient
-                    .get(endPoint)
-                    .then((res: any) => {
-                        const root = new RootRouteMap();
-                        const routeMaps = res.data.data.records.map((r: any) => {
-                            return mapDataToRoute(r, root);
-                        });
-                        root.children = routeMaps;
-                        resolve(root);
-                    })
-                    .catch((err: Error) => reject(err));
+      return new Promise((resolve, reject) => {
+        httpClient
+          .get(endPoint)
+          .then((res: any) => {
+            const root = new RootRouteMap();
+            const routeMaps = res.data.data.records.map((r: any) => {
+              return mapDataToRoute(r, root);
             });
-        },
-        queryList({ resource, page, size }: { resource: string; page: number; size: number }) {
-            const endPoint = `/${gateway}-cms/api/queryArchivesList`;
-            return new Promise((resolve, reject) => {
-                httpClient
-                    .post(endPoint, {
-                        entity: {
-                            channelNo: resource
-                        },
-                        param: {
-                            pageSize: size,
-                            pageNum: page
-                        }
-                    })
-                    .then((res: any) => {
-                        resolve({
-                            records: listMapper(res.data.data.records),
-                            pages: res.data.data.pages,
-                            page: res.data.data.current,
-                            total: res.data.data.total,
-                            $$rawData: res.data.data
-                        });
-                    })
-                    .catch((err: any) => {
-                        reject(err);
-                    });
+            root.children = routeMaps;
+            resolve(root);
+          })
+          .catch((err: Error) => reject(err));
+      });
+    },
+    queryList({ resource, page, size }: { resource: string; page: number; size: number }) {
+      const endPoint = `/${gateway}/api/queryArchivesList`;
+      return new Promise((resolve, reject) => {
+        httpClient
+          .post(endPoint, {
+            entity: {
+              channelNo: resource
+            },
+            param: {
+              pageSize: size,
+              pageNum: page
+            }
+          })
+          .then((res: any) => {
+            resolve({
+              records: listMapper(res.data.data.records),
+              pages: res.data.data.pages,
+              page: res.data.data.current,
+              total: res.data.data.total,
+              $$rawData: res.data.data
             });
-        },
-        queryOneById(resource: string, path = 'queryArchivesById') {
-            return new Promise((resolve, reject) => {
-                return httpClient
-                    .get(`/${gateway}-cms/api/${path}/${resource}`)
-                    .then((res: any) => {
-                        resolve({
-                            data: listOneMapper(res.data.data)
-                            // $$rawData: res.data.data
-                        });
-                    })
-                    .catch((err: any) => {
-                        reject(err);
-                    });
+          })
+          .catch((err: any) => {
+            reject(err);
+          });
+      });
+    },
+    queryOneById(resource: string, path = 'queryArchivesById') {
+      return new Promise((resolve, reject) => {
+        return httpClient
+          .get(`/${gateway}/api/${path}/${resource}`)
+          .then((res: any) => {
+            resolve({
+              data: listOneMapper(res.data.data)
+              // $$rawData: res.data.data
             });
-        },
-        likeIt(resource: string) {
-            return new Promise((resolve, reject) => {
-                return httpClient
-                    .get(`/${gateway}-cms/api/doStarByArcId/${resource}`)
-                    .then((res: any) => {
-                        resolve({
-                            code: res.data.code
-                        });
-                    })
-                    .catch((err: any) => {
-                        reject(err);
-                    });
+          })
+          .catch((err: any) => {
+            reject(err);
+          });
+      });
+    },
+    likeIt(resource: string) {
+      return new Promise((resolve, reject) => {
+        return httpClient
+          .get(`/${gateway}/api/doStarByArcId/${resource}`)
+          .then((res: any) => {
+            resolve({
+              code: res.data.code
             });
-        },
-        unLikeIt(resource: string) {
-            return new Promise((resolve, reject) => {
-                return httpClient
-                    .get(`/${gateway}-cms/api/cancelStarByActId/${resource}`)
-                    .then((res: any) => {
-                        resolve({
-                            code: res.data.code
-                        });
-                    })
-                    .catch((err: any) => {
-                        reject(err);
-                    });
+          })
+          .catch((err: any) => {
+            reject(err);
+          });
+      });
+    },
+    unLikeIt(resource: string) {
+      return new Promise((resolve, reject) => {
+        return httpClient
+          .get(`/${gateway}/api/cancelStarByActId/${resource}`)
+          .then((res: any) => {
+            resolve({
+              code: res.data.code
             });
-        }
-    };
+          })
+          .catch((err: any) => {
+            reject(err);
+          });
+      });
+    }
+  };
 };

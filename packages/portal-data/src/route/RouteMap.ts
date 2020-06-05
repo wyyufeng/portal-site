4527f104587804a5d8af0ce57d6d718b5b437770
Chenhaoran <1300063762@qq.com>
2020-06-05 15:33:49 +0800

    添加 栏目数组转对象

--- packages/portal-data/src/route/RouteMap.ts
+++ packages/portal-data/src/route/RouteMap.ts
@@ -30,13 +30,22 @@ function toPath(route: IRoute): string {
 }
 
 const _flatResultCache: IRoute[] = [];
+const _flatToObjectResult: object = {};
 function _flatRoutes(root: any) {
     const children = root.children;
     if (children.length > 0) {
         !root.isRoot && _flatResultCache.push(root);
-        return children.map((i: any) => _flatRoutes(i));
+        children.map((i: any) => _flatRoutes(i));
     }
     !root.isRoot && _flatResultCache.push(root);
+}
+function _flatToObject(root: any) {
+    const children = root.children;
+    if (children.length > 0) {
+        !root.isRoot && (_flatToObjectResult[root.route] = root);
+        children.map((i: any) => _flatToObject(i));
+    }
+    !root.isRoot && (_flatToObjectResult[root.route] = root);
 }
 
 export class RootRouteMap implements IRootRouteMap {
@@ -53,6 +62,11 @@ export class RootRouteMap implements IRootRouteMap {
         if (_flatResultCache.length > 0) return _flatResultCache;
         _flatRoutes(this);
         return _flatResultCache;
+    }
+    flatToObject(): object {
+        if (Object.keys(_flatToObjectResult).length > 0) return _flatToObjectResult;
+        _flatToObject(this);
+        return _flatToObjectResult;
     }
     findByPath(path: string): IRoute | undefined {
         return this.flat().find((route) => route.path === path);

92bcd04cc5b678315e03b584929dec5a0a80ce3e
wyyufeng <wyyufeng@gmail.com>
2020-06-08 10:14:05 +0800

    fix:修改package.json

--- packages/portal-data/lib/httpClient.d.ts
+++
@@ -1,3 +0,0 @@
-import { AxiosInstance } from 'axios';
-export declare function createHttpClient(baseURL: string, appId: string, appKey: string): AxiosInstance;
-export declare function encryptApiKey(key: string): string;

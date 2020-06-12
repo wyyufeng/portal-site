a24497dcea476618b65c4f31d23de8a3e37d852e
Chenhaoran <1300063762@qq.com>
2020-06-12 15:23:08 +0800

    fix pages必须为number

--- packages/portal-core/src/hooks/useQueryList.ts
+++ packages/portal-core/src/hooks/useQueryList.ts
@@ -3,33 +3,52 @@ import { ListItem } from '@portal-site/types';
 import { ListResponse } from '../dataProvider/DataProviderContext';
 
 export type QueryListParams = {
-  resource: string
-  pagination: {
-    page: number,
-    size: number
-  },
-  params?: any
-  formatResult?: (res: any) => any,
-  onSuccess?: (data: any, params: any) => void,
-  onError?: (error: any, params: any) => void
-}
+    resource: string;
+    pagination: {
+        page: number;
+        size: number;
+    };
+    params?: any;
+    formatResult?: (res: any) => any;
+    onSuccess?: (data: any, params: any) => void;
+    onError?: (error: any, params: any) => void;
+};
 export type QueryListResult = {
-  records: undefined | Array<ListItem>;
-  total?: number;
-  error: any;
-  loading: boolean;
-  pages: undefined | number;
-  page: undefined | number;
-}
+    records: undefined | Array<ListItem>;
+    total?: number;
+    error: any;
+    loading: boolean;
+    pages: undefined | number;
+    page: undefined | number;
+};
 
-const useQueryList = ({ resource, pagination, onError, onSuccess, formatResult, params }: QueryListParams): QueryListResult => {
-  const { data, error, loading } = useQuery<ListResponse>({
-    api: 'queryList', options: {
-      resource, size: pagination.size, page: pagination.page, params
-    }, formatResult, onSuccess, onError
-  })
-  return {
-    records: data?.records, error, loading, pages: data?.pages, total: data?.total, page: data?.page
-  }
-}
-export default useQueryList
+const useQueryList = ({
+    resource,
+    pagination,
+    onError,
+    onSuccess,
+    formatResult,
+    params
+}: QueryListParams): QueryListResult => {
+    const { data, error, loading } = useQuery<ListResponse>({
+        api: 'queryList',
+        options: {
+            resource,
+            size: pagination.size,
+            page: pagination.page,
+            params
+        },
+        formatResult,
+        onSuccess,
+        onError
+    });
+    return {
+        records: data?.records,
+        error,
+        loading,
+        pages: data?.pages ? data.pages : 0,
+        total: data?.total,
+        page: data?.page
+    };
+};
+export default useQueryList;

f1340e693883456eda1bb4abec0633de896755df
Chenhaoran <1300063762@qq.com>
2020-06-16 12:20:57 +0800

    gx

--- packages/portal-core/src/hooks/useQueryList.ts
+++ packages/portal-core/src/hooks/useQueryList.ts
@@ -4,55 +4,55 @@ import { ListItem } from '@portal-site/types';
 import { ListResponse } from '../dataProvider/DataProviderContext';
 
 export type QueryListParams = {
-    resource: string;
-    pagination: {
-        page: number;
-        size: number;
-    };
-    params?: any;
-    formatResult?: (res: any) => any;
-    onSuccess?: (data: any, params: any) => void;
-    onError?: (error: any, params: any) => void;
+  resource: string;
+  pagination: {
+    page: number;
+    size: number;
+  };
+  params?: any;
+  formatResult?: (res: any) => any;
+  onSuccess?: (data: any, params: any) => void;
+  onError?: (error: any, params: any) => void;
 };
 export type QueryListResult = {
-    records: undefined | Array<ListItem>;
-    total?: number;
-    error: any;
-    loading: boolean;
-    pages: undefined | number;
-    page: undefined | number;
+  records: undefined | Array<ListItem>;
+  total?: number;
+  error: any;
+  loading: boolean;
+  pages: undefined | number;
+  page: undefined | number;
 };
 
 const useQueryList = ({
-    resource,
-    pagination,
-    onError,
-    onSuccess,
-    formatResult,
-    params
+  resource,
+  pagination,
+  onError,
+  onSuccess,
+  formatResult,
+  params
 }: QueryListParams): QueryListResult => {
-    const memoOptions = useMemo(() => {
-        return {
-            resource,
-            size: pagination.size,
-            page: pagination.page,
-            params
-        };
-    }, [resource, pagination.size, pagination.page, params]);
-    const { response, error, loading } = useQuery<ListResponse>({
-        api: 'queryList',
-        options: memoOptions,
-        formatResult,
-        onSuccess,
-        onError
-    });
+  const memoOptions = useMemo(() => {
     return {
-        records: response?.records ?? [],
-        error,
-        loading,
-        pages: response?.pages ?? 0,
-        total: response?.total,
-        page: response?.page ?? 0
+      resource,
+      size: pagination.size,
+      page: pagination.page,
+      params
     };
+  }, [resource, pagination.size, pagination.page, params]);
+  const { response, error, loading } = useQuery<ListResponse>({
+    api: 'queryList',
+    options: memoOptions,
+    formatResult,
+    onSuccess,
+    onError
+  });
+  return {
+    records: response?.records ?? [],
+    error,
+    loading,
+    pages: response?.pages ?? 0,
+    total: response?.total,
+    page: response?.page ?? 0
+  };
 };
 export default useQueryList;

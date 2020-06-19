1e69cb8f1c1be178f841233d1b0211c1359b288c
Chenhaoran <1300063762@qq.com>
2020-06-19 18:02:13 +0800

    html2text

--- packages/portal-data/src/mapDataToRoute.ts
+++ packages/portal-data/src/mapDataToRoute.ts
@@ -2,15 +2,14 @@ import { IRoute } from '@portal-site/types';
 import { RouteMap } from './route';
 
 export default function mapDataToRoute(data: any, parent: any): IRoute {
-    const route = new RouteMap();
-    const children = data.subChannel
-        ? data.subChannel.map((i: any) => mapDataToRoute(i, route))
-        : [];
-    route.children = children;
-    route.name = data.channelName;
-    route.route = data.channelNo;
-    route.parent = parent;
-    route.description = data.description;
-    route.isHome = data.channelNo === 'Home';
-    return route;
+  const route = new RouteMap();
+  const children = data.subChannel ? data.subChannel.map((i: any) => mapDataToRoute(i, route)) : [];
+  route.id = data.channelId;
+  route.children = children;
+  route.name = data.channelName;
+  route.route = data.channelNo;
+  route.parent = parent;
+  route.description = data.description;
+  route.isHome = data.channelNo === 'Home';
+  return route;
 }
--- packages/portal-data/src/util.ts
+++ packages/portal-data/src/util.ts
@@ -1,29 +1,35 @@
 export const getImgFromHtml = (content: string = ''): string => {
-    if (!content) return '';
-    //   匹配图片
-    const imgReg = /<img.*?(?:>|\/>)/gi;
-    //   匹配src
-    const srcReg = /src=['"]?([^'"]*)['"]?/i;
-    const arr = content.match(imgReg);
-    if (arr === null || arr.length < 0) return '';
-    //   获取第一张图片的src
-    const src = arr[0].match(srcReg);
-    return src ? src[1] : '';
+  if (!content) return '';
+  //   匹配图片
+  const imgReg = /<img.*?(?:>|\/>)/gi;
+  //   匹配src
+  const srcReg = /src=['"]?([^'"]*)['"]?/i;
+  const arr = content.match(imgReg);
+  if (arr === null || arr.length < 0) return '';
+  //   获取第一张图片的src
+  const src = arr[0].match(srcReg);
+  return src ? src[1] : '';
 };
 
 export const html2text = (
-    content: string,
-    {
-        length
-    }: {
-        length: number;
-        wordBreak: boolean;
-    } = {
-        length: 100,
-        wordBreak: false
-    }
+  content: string,
+  {
+    length
+  }: {
+    length: number;
+    wordBreak: boolean;
+  } = {
+    length: 100,
+    wordBreak: false
+  }
 ): string => {
-    const s = content?.replace(/<[^>]+>|&[^>]+;/g, '').trim();
-    if (s === '' || s === undefined) return '';
-    return s.substr(0, length) + '...';
+  let quot = content
+    .replace(/&rdquo;/g, '”')
+    .replace(/&ldquo;/g, '“')
+    .replace(/&quot;/g, '"')
+    .replace(/<[^>]+>|&[^>]+;/g, '')
+    .trim();
+  const s = quot?.replace(/<[^>]+>|&[^>]+;/g, '').trim();
+  if (s === '' || s === undefined) return '';
+  return s.substr(0, length) + '...';
 };

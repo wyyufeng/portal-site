57ef41d3e3ac9a4d5c34b88977cbe1767bd6ef99
wyyufeng <7765074+wyyufeng@user.noreply.gitee.com>
2020-09-27 11:52:45 +0800

    fix: 修复portal-site/data 模块未导出的问题

--- packages/portal-data/src/index.ts
+++ packages/portal-data/src/index.ts
@@ -3,4 +3,7 @@ export * from './util';
 export * from './mapper';
 export * from './httpClient';
 export * from './model';
+export * from './route'
+export * from './mapDataToRoute'
+export * from './mapper'
 export { cmsDataProvider };
